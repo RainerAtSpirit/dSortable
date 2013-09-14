ded743bc3626ba5e1566f2c9fffa7f36a3607e4b
RainerAtSpirit <R.Wittmann@spirit.de>
2013-09-14 18:18:45 +0200

    refactor table and student into their own modules

--- app/global.js
+++ app/global.js
@@ -1,7 +1,7 @@
 ﻿define(['durandal/system', 'knockout'],
   function( system, ko ) {
 
-      var dFiddleRepoSource = 'https://github.com/dFiddle/dFiddle-2.0/tree/gh-pages/app/';
+      var dFiddleRepoSource = 'http://rainerat.spirit.de/dSortable/tree/gh-pages/app/';
 
       var isDFiddle = ko.observable(false);
       var dFiddleRepoUrl = ko.observable('');
