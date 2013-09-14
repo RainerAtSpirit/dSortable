522ce4a05519affabb98ea2e2d72cd2aab677679
RainerAtSpirit <R.Wittmann@spirit.de>
2013-09-14 18:20:25 +0200

    lowercase filename to make github happy

---
+++ app/extras/sortable/student.js
@@ -0,0 +1,12 @@
+﻿define(['knockout'], function( ko ) {
+
+    var ctor = function( id, name, gender ) {
+        this.id = id;
+        this.name = ko.observable(name);
+        this.gender = gender;
+    };
+
+
+    return ctor;
+
+});
---
+++ app/extras/sortable/table.js
@@ -0,0 +1,10 @@
+﻿define(['knockout'], function( ko ) {
+
+    var ctor = function( id, students ) {
+        this.students = ko.observableArray(students);
+        this.students.id = id;
+    };
+
+    return ctor;
+
+});
